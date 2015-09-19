89fc5517b84583451ea2323b91d754349435590d
Chris Ramón <rchristian.ramon@gmail.com>
2015-09-19 09:41:38 -0500

    add js basic-excercises

---
+++ workshops/js/basic-excercises/index.js
@@ -0,0 +1,63 @@
+// Ejemplo.
+// 0) Crear una variable id con valor 10
+// Finalmente imprimir en consola la variable id;
+
+var id = 10;
+console.log(id);
+
+//
+// 1) Crear una variable year de tipo entero
+// y asignar como valor 2015.
+// Finalmente imprimir en la consola la variable year.
+
+
+
+// 2) Crear una variable name de tipo string
+// y asignar como valor tu nombre.
+// Finalmente imprimir en la consola la variable name.
+
+
+
+// 3) Crear una variable ids de tipo arreglo de enteros
+// y asignar como valores 10, 20 y 30.
+// Finalmente imprimir en la consola la variable ids.
+
+
+
+// 4) Crear una variable names de tipo arreglo de strings
+// y asignar como valores 'A', 'C' y 'D'.
+// Finalmente imprimir en la consola la variable names.
+
+
+
+// 6) Crear una variable product de tipo static object
+// y asignar como propiedades: id con valor 100 y
+// name con valor EFG.
+// Finalmente imprimir en la consola la variable product.
+
+
+
+// 7) Escribir una bucle que imprima en consola del 0 al 10.
+
+
+
+// 8) Escribir una función que imprima en consola el string Hola Amigos.
+
+
+
+// 9) Escribir una función que recibe 2 parametros A y B e imprime
+// en consola el resultado de la suma de A y B.
+
+
+
+// 10) Escribir una función que recibe 2 parámetros:
+// A que es un arreglo de enteros y B que es un entero
+// Imprimir en consola cuantas veces B esta presente en el arreglo A.
+//
+// Test 1:
+// ejemplo10([10, 20, 30, 10], 10)
+// resultado: 2
+//
+// Test 2:
+// ejemplo10([10, 20, 30, 10], 20)
+// resultado: 1
